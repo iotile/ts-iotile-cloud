3e5d4687df6489b1ecd72ffa5beaa6a74aa57def
Kaylie DeHart <kaylie.dehart@gmail.com>
2018-10-10 16:58:18 -0700

    external

--- rollup.config.ts
+++ rollup.config.ts
@@ -16,7 +16,7 @@ export default {
   ],
   sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ['typescript-logging'],
   watch: {
     include: 'src/**',
   },
