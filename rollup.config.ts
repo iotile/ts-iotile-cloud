48fd0eebc6de698bf29b3f23100cc6b58fb3b37d
Tim Burke <timburke@alumni.rice.edu>
2019-01-02 15:01:53 -0800

    Move iotile-common to peer dependency

--- rollup.config.ts
+++ rollup.config.ts
@@ -16,7 +16,7 @@ export default {
   ],
   sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['typescript-logging'],
+  external: ['typescript-logging', "@iotile/iotile-common"],
   watch: {
     include: 'src/**',
   },
